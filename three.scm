0074a795cdeabbd71725078768b38493c6edf1a9
Rajkumar Reddy <mesg.raj@gmail.com>
2025-12-24 10:59:28 +0530

    Create three.scm

---
+++ three.scm
@@ -0,0 +1,9 @@
+(define (expte var)
+  (if (negative?(- var 4))
+    (begin 
+      (display "vvavriable is close to zero")
+      (display "ca i write 2 statements in if"))
+    (display (expt 5 100))))
+
+
+(expte 3)
